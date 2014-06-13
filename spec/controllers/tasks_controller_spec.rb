45f847fc9005f6182a08bde8bf4d9846f23f7d28
Sid Tantia <sstantia@gmail.com>
2014-06-13 10:11:52 -0400

    rspec tests for tasks controller for index and create working

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -11,19 +11,30 @@ describe TasksController do
 		# end
 
 		it 'assigns collection of tasks' do
-			sign_in
-			current_user = user
-			f = FactoryGirl.create(:task)
+			
+			# current_user = user
+			# current_user = :current_user
+			u = FactoryGirl.create(:user)
+			sign_in(u)
+			f = FactoryGirl.create(:task, :user_id => u.id)
 			get :index
 			assigns(:tasks).count.should eq(1)
 		end
 
 		it 'renders the index page' do
+			current_user = FactoryGirl.create(:user)
+			sign_in(current_user)
+			get :index
+			response.should be_success
 		end
 	end
 
 	describe "POST #create" do
 		it 'creates a new task' do
+			current_user = FactoryGirl.create(:user)
+			sign_in(current_user)
+			expect{
+				post :create, task: FactoryGirl.attributes_for(:task)}.to change(Task, :count).by(1)
 		end
 
 	end
---
+++ spec/factories/user.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+	factory(:user) do
+		first_name "asiudhfiuhasdf"
+		last_name "asdfasdgasdha"
+		email "asdfasdf@asdghasdf.com"
+		password "asdfashgas"
+		password_confirmation "asdfashgas"
+	end
+
+end
