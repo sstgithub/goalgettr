8787485a5e519dae7b624c46285dc072795fbafd
Sid Tantia <sstantia@gmail.com>
2014-06-15 13:54:14 -0400

    Added format_date and format_time helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def format_date(date)
+    if date
+      date.strftime("%B %d, %Y")
+    else
+      "Date not specified"
+    end
+  end
+
+  def format_time(time)
+  	if time
+  		time.strftime("%l:%M %p")
+  	else
+  		"Time not specified"
+  	end
+  end
 end
