c6742251a74a70b612f9e1b2055e65c0a74dd31f
Sid Tantia <sstantia@gmail.com>
2014-06-15 13:54:31 -0400

    Added show, update, and edit methods to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,39 @@ class TasksController < ApplicationController
 		@task = Task.new
 	end
 
+	def show
+		@task = Task.find(params[:id])
+	end
+
 	def new
 		@task = Task.new
+	end
+
+	def edit
+		@task = Task.find(params[:id])
 	end
 
 	def create
 		@task = Task.new(task_params)
 		if @task.save
+			redirect_to tasks_path
+		else
+			redirect_to :back
+		end
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		if @task.update_attributes(task_params)
+			redirect_to task_path(params[:id])
+		else
+			redirect_to :back
+		end
+	end
+
+	def destroy
+		@task = Task.find(params[:id])
+		if @task.destroy
 			redirect_to tasks_path
 		else
 			redirect_to :back
