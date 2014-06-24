077098ef4232866f5e5b3323543c2936bcdf9254
Sid Tantia <sstantia@gmail.com>
2014-06-23 22:15:41 -0400

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,15 @@ class TasksController < ApplicationController
 			@i1u1_tasks = @tasks.where(importance: 1, urgency: 1)
 			@i0u0_tasks = @tasks.where(importance: 0, urgency: 0)
 			@i0u1_tasks = @tasks.where(importance: 0, urgency: 1)
+			@hash = Gmaps4rails.build_markers(@tasks) do |task, marker|
+			  marker.lat task.latitude
+			  marker.lng task.longitude
+			  marker.infowindow "<a target='blank' href=#{task_path(task)}>#{task.task_name}: </a>"
+			  marker.json({ title: task.task_name })
+			end
 		end
 		@task = Task.new
-		@hash = Gmaps4rails.build_markers(@tasks) do |task, marker|
-		  marker.lat task.latitude
-		  marker.lng task.longitude
-		  marker.infowindow "<a target='blank' href=#{task_path(task)}>#{task.task_name}: </a>"
-		  marker.json({ title: task.task_name })
-		end
+		
 	end
 
 	def show
