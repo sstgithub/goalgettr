0c4ad9988c515e264a7e595fd117a0eb35fa6035
Sid Tantia <sstantia@gmail.com>
2014-06-29 17:34:24 -0400

    More DRYing up

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -9,22 +9,18 @@ module TasksHelper
 
 	def travel_time(first_task, second_task)
 		if first_task.street_address && second_task.street_address
-      if first_task.street_address.include?("-")
-        @t_street_address = first_task.street_address.split("-")[1]
-      else
-      	@t_street_address = first_task.street_address
-      end
-      if second_task.street_address.include?("-")
-      	@a_street_address = second_task.street_address.split("-")[1]
-      else
-      	@a_street_address = second_task.street_address
-      end
+      t_street_address = first_task.street_address.split("-").last
+      a_street_address = second_task.street_address.split("-").last
     end
-    if @t_street_address && @a_street_address
-      response = HTTParty.get("http://dev.virtualearth.net/REST/V1/Routes?wp.0=#{format_string(@t_street_address)}%20#{format_string(first_task.city)},#{format_string(first_task.state)}&wp.1=#{format_string(@a_street_address)}%20#{format_string(second_task.city)},#{format_string(second_task.state)}&optmz=timeWithTraffic&key=Ah_f4EtXujIKRwM2DMfcrhaL_XJ0G_Ob_Yyy1nlDf0X_LYGhJilPBjWSfI91E3MV")
+
+    if t_street_address && a_street_address
+      response = HTTParty.get("http://dev.virtualearth.net/REST/V1/Routes?wp.0=#{format_string(t_street_address)}%20#{format_string(first_task.city)},#{format_string(first_task.state)}&wp.1=#{format_string(a_street_address)}%20#{format_string(second_task.city)},#{format_string(second_task.state)}&optmz=timeWithTraffic&key=Ah_f4EtXujIKRwM2DMfcrhaL_XJ0G_Ob_Yyy1nlDf0X_LYGhJilPBjWSfI91E3MV")
 		end
-    if response
-      response["resourceSets"][0]["resources"][0]["travelDurationTraffic"]
+
+    if response["resourceSets"][0]["resources"][0]["travelDurationTraffic"]
+      "#{distance_of_time_in_words(response["resourceSets"][0]["resources"][0]["travelDurationTraffic"])} driving time"
+    else 
+    	"Travel Time Unavailable"
     end
   end
 
